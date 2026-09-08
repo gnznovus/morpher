54e93db419df9f7787b9a70886ec313becd50254
gnznovus <gnznovus.dev@gmail.com>
2026-09-08 11:27:51 +0700

    Add CSS renderer foundation

---
+++ src/morpher/renderers/css.py
@@ -0,0 +1,9 @@
+from morpher.ir.nodes import DesignNode
+
+
+def render_css(root: DesignNode) -> str:
+    """Render Design IR to CSS.
+
+    Prototype placeholder for the first conversion slice.
+    """
+    raise NotImplementedError("CSS renderer not implemented yet")
