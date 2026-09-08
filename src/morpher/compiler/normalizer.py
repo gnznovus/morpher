89d684d472889277dcf57fcbb4465abf2959b149
gnznovus <gnznovus.dev@gmail.com>
2026-09-08 11:27:40 +0700

    Add compiler normalizer foundation

---
+++ src/morpher/compiler/normalizer.py
@@ -0,0 +1,9 @@
+from morpher.ir.nodes import DesignNode
+
+
+def normalize(source) -> DesignNode:
+    """Convert adapter output into the shared Design IR.
+
+    Prototype placeholder. Concrete normalization begins with Figma JSON.
+    """
+    raise NotImplementedError("Normalizer not implemented yet")
