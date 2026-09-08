78d866ff06c9c0facf2b3b96bb65928ad016eb4d
gnznovus <gnznovus.dev@gmail.com>
2026-09-08 11:27:45 +0700

    Add HTML renderer foundation

---
+++ src/morpher/renderers/html.py
@@ -0,0 +1,9 @@
+from morpher.ir.nodes import DesignNode
+
+
+def render_html(root: DesignNode) -> str:
+    """Render Design IR to HTML.
+
+    Prototype placeholder for the first conversion slice.
+    """
+    raise NotImplementedError("HTML renderer not implemented yet")
