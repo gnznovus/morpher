b3f1a5306121bc11cd02fa22f58e7f82c8c9f1d0
gnznovus <gnznovus.dev@gmail.com>
2026-09-11 16:17:57 +0700

    Export font registry cache helpers

--- src/morpher/fonts/__init__.py
+++ src/morpher/fonts/__init__.py
@@ -1,9 +1,13 @@
+from morpher.fonts.cache import load_font_registry_cache, save_font_registry_cache
 from morpher.fonts.model import FontFace, FontMetadata, FontSource, UnresolvedFontSource
 from morpher.fonts.registry import (
     FontRegistry,
     current_font_registry,
+    ensure_font_face,
     gather_fonts,
+    load_cached_font_registry,
     refresh_font_registry,
+    set_current_font_registry,
 )
 
 __all__ = [
@@ -13,6 +17,11 @@ __all__ = [
     "FontSource",
     "UnresolvedFontSource",
     "current_font_registry",
+    "ensure_font_face",
     "gather_fonts",
+    "load_cached_font_registry",
+    "load_font_registry_cache",
     "refresh_font_registry",
+    "save_font_registry_cache",
+    "set_current_font_registry",
 ]
