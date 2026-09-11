8d3ea3e3d0735dfef782987d85e11326e1252dfa
gnznovus <gnznovus.dev@gmail.com>
2026-09-11 15:46:18 +0700

    Expose font registry API

---
+++ src/morpher/fonts/__init__.py
@@ -0,0 +1,11 @@
+from morpher.fonts.model import FontFace, FontMetadata, FontSource, UnresolvedFontSource
+from morpher.fonts.registry import FontRegistry, gather_fonts
+
+__all__ = [
+    "FontFace",
+    "FontMetadata",
+    "FontRegistry",
+    "FontSource",
+    "UnresolvedFontSource",
+    "gather_fonts",
+]
