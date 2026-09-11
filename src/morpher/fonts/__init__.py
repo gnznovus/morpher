db22a62b88a224f98e78af1e15750469b1f44c8c
gnznovus <gnznovus.dev@gmail.com>
2026-09-11 16:59:51 +0700

    Expose cache-aware font resolution

--- src/morpher/fonts/__init__.py
+++ src/morpher/fonts/__init__.py
@@ -9,7 +9,14 @@ from morpher.fonts.registry import (
     refresh_font_registry,
     set_current_font_registry,
 )
-from morpher.fonts.resolver import FontRequest, FontResolution, resolve_font
+from morpher.fonts.resolver import (
+    FontRequest,
+    FontResolution,
+    font_request_from_intent,
+    resolve_font,
+    resolve_font_intent,
+    resolve_font_with_cache,
+)
 
 __all__ = [
     "FontFace",
@@ -21,11 +28,14 @@ __all__ = [
     "UnresolvedFontSource",
     "current_font_registry",
     "ensure_font_face",
+    "font_request_from_intent",
     "gather_fonts",
     "load_cached_font_registry",
     "load_font_registry_cache",
     "refresh_font_registry",
     "resolve_font",
+    "resolve_font_intent",
+    "resolve_font_with_cache",
     "save_font_registry_cache",
     "set_current_font_registry",
 ]
