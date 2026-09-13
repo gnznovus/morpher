4cbf37f8fa82d8dd749d4665682835e310acaf7a
gnznovus <gnznovus.dev@gmail.com>
2026-09-13 08:37:37 +0700

    Add protected deployment listing endpoint

--- wp-content/plugins/morpher-plugin/includes/class-morpher-rest.php
+++ wp-content/plugins/morpher-plugin/includes/class-morpher-rest.php
@@ -7,6 +7,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Morpher_REST {
     const NAMESPACE = 'morpher/v1';
 
+    private $deployments;
+
+    public function __construct( Morpher_Deployment $deployments ) {
+        $this->deployments = $deployments;
+    }
+
     public function register() {
         add_action( 'rest_api_init', array( $this, 'register_routes' ) );
     }
@@ -19,6 +25,16 @@ class Morpher_REST {
                 'methods'             => WP_REST_Server::READABLE,
                 'callback'            => array( $this, 'health' ),
                 'permission_callback' => '__return_true',
+            )
+        );
+
+        register_rest_route(
+            self::NAMESPACE,
+            '/deployments',
+            array(
+                'methods'             => WP_REST_Server::READABLE,
+                'callback'            => array( $this, 'deployments' ),
+                'permission_callback' => array( $this, 'manage_options_permission' ),
             )
         );
     }
@@ -46,8 +62,43 @@ class Morpher_REST {
                 ),
                 'capabilities' => array(
                     'health',
+                    'deployments:list',
                 ),
             )
+        );
+    }
+
+    public function deployments() {
+        $items = array_map(
+            function ( $row ) {
+                return array(
+                    'deployment'  => isset( $row['deployment'] ) ? (string) $row['deployment'] : '',
+                    'slug'        => isset( $row['slug'] ) ? (string) $row['slug'] : '',
+                    'title'       => isset( $row['title'] ) ? (string) $row['title'] : '',
+                    'status'      => isset( $row['status'] ) ? (string) $row['status'] : '',
+                    'template_id' => isset( $row['id'] ) ? (int) $row['id'] : 0,
+                    'error'       => isset( $row['error'] ) ? (string) $row['error'] : '',
+                );
+            },
+            $this->deployments->rows()
+        );
+
+        return rest_ensure_response(
+            array(
+                'deployments' => array_values( $items ),
+            )
+        );
+    }
+
+    public function manage_options_permission() {
+        if ( current_user_can( 'manage_options' ) ) {
+            return true;
+        }
+
+        return new WP_Error(
+            'morpher_rest_forbidden',
+            'You are not allowed to manage Morpher deployments.',
+            array( 'status' => rest_authorization_required_code() )
         );
     }
 }
