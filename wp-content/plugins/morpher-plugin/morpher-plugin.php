ba64a33113ac7c6ae38db88880ca0b054393f469
gnznovus <gnznovus.dev@gmail.com>
2026-09-13 08:13:07 +0700

    Load Morpher REST integration

--- wp-content/plugins/morpher-plugin/morpher-plugin.php
+++ wp-content/plugins/morpher-plugin/morpher-plugin.php
@@ -9,11 +9,14 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+define( 'MORPHER_PLUGIN_VERSION', '0.4.0' );
+
 $morpher_root = plugin_dir_path( __FILE__ );
 
 require_once $morpher_root . 'includes/class-morpher-deployment.php';
 require_once $morpher_root . 'includes/class-morpher-assets.php';
 require_once $morpher_root . 'includes/class-morpher-diagnostics.php';
+require_once $morpher_root . 'includes/class-morpher-rest.php';
 require_once $morpher_root . 'includes/class-morpher-admin.php';
 require_once $morpher_root . 'includes/class-morpher-plugin.php';
 
