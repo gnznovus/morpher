50af4a887ab177c886285cd505b93145c932217e
gnznovus <gnznovus.dev@gmail.com>
2026-09-13 07:07:32 +0700

    Expose Design IR validation API

--- src/morpher/ir/__init__.py
+++ src/morpher/ir/__init__.py
@@ -1,1 +1,5 @@
 """Morpher shared design intermediate representation."""
+
+from .validation import IRDiagnostic, ValidationResult, validate_design_ir
+
+__all__ = ["IRDiagnostic", "ValidationResult", "validate_design_ir"]
