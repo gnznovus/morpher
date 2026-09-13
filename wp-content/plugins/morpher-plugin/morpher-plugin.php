4bf906fed1c3a93a97bbfbdd84f1328b045c35c3
gnznovus <gnznovus.dev@gmail.com>
2026-09-13 08:02:06 +0700

    Load Morpher diagnostics service

--- wp-content/plugins/morpher-plugin/morpher-plugin.php
+++ wp-content/plugins/morpher-plugin/morpher-plugin.php
@@ -13,6 +13,7 @@ $morpher_root = plugin_dir_path( __FILE__ );
 
 require_once $morpher_root . 'includes/class-morpher-deployment.php';
 require_once $morpher_root . 'includes/class-morpher-assets.php';
+require_once $morpher_root . 'includes/class-morpher-diagnostics.php';
 require_once $morpher_root . 'includes/class-morpher-admin.php';
 require_once $morpher_root . 'includes/class-morpher-plugin.php';
 
